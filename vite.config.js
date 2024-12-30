f0dfc1739bef404f5873aa361e759a95d4c6c957
aadarsh1503 <aadarshchauhan35@gmail.com>
2024-12-30 16:58:00 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: '', // The base URL of your API
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove `/api` prefix when forwarding the request
+      },
+    },
+  },
 })
