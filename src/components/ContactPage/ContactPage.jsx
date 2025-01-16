0d84b01cb7a0cff7cc5db3453b73757d2cc7cc61
aadarsh1503 <aadarshchauhan35@gmail.com>
2025-01-16 19:42:38 +0530

    done

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -16,25 +16,31 @@ const ContactPage = () => {
       </h1>
 
       {/* Map Section */}
-      <div className="flex justify-center ">
-      <iframe
-          title="Google Map"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14435.236249565674!2d50.5237206!3d26.1868843!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49afd0213a19d3%3A0x2f892ffe9e6f0385!2sGlobal%20Vision%20Solutions!5e0!3m2!1sen!2sin!4v1731936574918!5m2!1sen!2sin"
-          width="100%"
-          height="400"
-          className="rounded-lg shadow-lg w-full max-w-4xl"
-          allowFullScreen
-        ></iframe>
-      </div>
+      <div className=''>
+      <div className="flex p-6 shadow-custom rounded-xl flex-col w-3/4 ml-[180px] items-center space-y-4 mb-20">
+        
+  {/* Map Section */}
+  <iframe
+    title="Google Map"
+    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14435.236249565674!2d50.5237206!3d26.1868843!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49afd0213a19d3%3A0x2f892ffe9e6f0385!2sGlobal%20Vision%20Solutions!5e0!3m2!1sen!2sin!4v1731936574918!5m2!1sen!2sin"
+    width="100%"
+    height="400"
+    className="rounded-xl shadow-custom w-full max-w-4xl"
+    allowFullScreen
+  ></iframe>
+
+  {/* Address Section */}
+  <p className="text-left relative right-40 whitespace-nowrap   text-gray-700">
+    OFFICE 22, BLDG 661, RD 1208, BLOCK 712 - SALMABAD,
+    KINGDOM OF BAHRAIN
+  </p>
+</div>
+</div>
 
       {/* Contact Information Section */}
       <div className="max-w-4xl mx-auto mb-20 bg-white rounded-lg  p-6 space-y-6">
         {/* Address */}
-        <p className="text-left mb-20 text-gray-700">
-        OFFICE 22, BLDG 661,
-RD 1208, BLOCK 712 - SALMABAD,
-KINGDOM OF BAHRAIN
-        </p>
+       
 
         {/* Contact Sections */}
         <div className="grid grid-cols-1 md:grid-cols-3  gap-6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        'custom': 'rgba(17, 17, 26, 0.1) 0px 0px 16px',
+      },
       colors: {
         lorange: "#ff7404", // Corrected 'color' to 'colors'
       },
